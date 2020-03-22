ff36760c812d4bed0d1968aca0f0ad584a58b0e3
cap340 <spicard@cap340.fr>
2020-03-22 03:23:19 +0100

    Frontend: dashboard templates split

--- Block/Customer/Dashboard.php
+++ Block/Customer/Dashboard.php
@@ -3,7 +3,49 @@
 namespace Cap\Rma\Block\Customer;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Customer\Model\Session as CustomerSession;
 
 class Dashboard extends Template
 {
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+
+    /**
+     * Dashboard constructor.
+     *
+     * @param Template\Context $context
+     * @param CustomerSession $customerSession
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        CustomerSession $customerSession,
+        array $data = []
+    ) {
+        $this->customerSession = $customerSession;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoggedIn()
+    {
+        if ($this->customerSession->isLoggedIn()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Form action to send data in Form Controller.
+     *
+     * @return string
+     */
+    public function getFormAction()
+    {
+        return '/rma/customer/form';
+    }
 }
