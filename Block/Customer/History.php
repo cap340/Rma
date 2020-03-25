6fa9723d4431bb6ac9b64b1dbd9b6de7e89f0637
cap340 <spicard@cap340.fr>
2020-03-25 17:57:28 +0100

    Front tab history

--- Block/Customer/History.php
+++ Block/Customer/History.php
@@ -7,6 +7,8 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Magento\Framework\View\Element\Template;
 use Magento\Theme\Block\Html\Pager;
+use Magento\Customer\Model\Session as CustomerSession;
+use Cap\Rma\Model\Config\Source\Request\Status as RequestStatus;
 
 class History extends Template
 {
@@ -16,18 +18,34 @@ class History extends Template
     protected $requestCollection;
 
     /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+
+    /**
+     * @var RequestStatus
+     */
+    protected $requestStatus;
+
+    /**
      * Request constructor.
      *
      * @param Template\Context $context
      * @param Request $requestCollection
+     * @param CustomerSession $customerSession
+     * @param RequestStatus $requestStatus
      * @param array $data
      */
     public function __construct(
         Template\Context $context,
         Request $requestCollection,
+        CustomerSession $customerSession,
+        RequestStatus $requestStatus,
         array $data = []
     ) {
         $this->requestCollection = $requestCollection;
+        $this->customerSession = $customerSession;
+        $this->requestStatus = $requestStatus;
         parent::__construct($context, $data);
     }
 
@@ -45,7 +63,7 @@ class History extends Template
                 Pager::class,
                 'request.history.pager'
             )->setAvailableLimit(
-                [5 => 5, 10 => 10, 15 => 15, 20 => 20]
+                [10 => 10, 20 => 20, 30 => 30]
             )->setShowPerPage(
                 true
             )->setCollection(
@@ -66,17 +84,59 @@ class History extends Template
     }
 
     /**
+     * @return bool
+     */
+    public function isLoggedIn()
+    {
+        if ($this->customerSession->isLoggedIn()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * @return AbstractCollection
      */
     public function getRequestsHistory()
     {
         $page = ($this->getRequest()->getParam('p')) ? $this->getRequest()->getParam('p') : 1;
-        $pageSize = ($this->getRequest()->getParam('limit')) ? $this->getRequest()->getParam('limit') : 5;
+        $pageSize = ($this->getRequest()->getParam('limit')) ? $this->getRequest()->getParam('limit') : 10;
 
         $collection = $this->requestCollection->getCollection();
+        $collection->setOrder('created_at', 'desc');
         $collection->setPageSize($pageSize);
         $collection->setCurPage($page);
 
         return $collection;
+    }
+
+    /**
+     * @param $optionId
+     * @return mixed|null
+     */
+    public function getStatusOptionLabel($optionId)
+    {
+        $options = $this->requestStatus->toOptionArray();
+        foreach ($options as $key => $option) {
+            if (in_array($optionId, $option)) {
+                return $option['label'];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function checkUpdatedRequest(Request $request)
+    {
+        $createdAt = $request->getCreatedAt();
+        $updatedAt = $request->getUpdatedAt();
+        if ($updatedAt !== $createdAt) {
+            return true;
+        }
+        return false;
     }
 }
